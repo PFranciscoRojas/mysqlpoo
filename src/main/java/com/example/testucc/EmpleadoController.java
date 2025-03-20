e64ef59900a9d861598585408b7b024b0dbea99c
pfranciscorojas <franciscorojas92@gmail.com>
2025-03-20 18:32:45 -0500

    add anotations get and post

--- src/main/java/com/example/testucc/EmpleadoController.java
+++ src/main/java/com/example/testucc/EmpleadoController.java
@@ -3,19 +3,30 @@ package com.example.testucc;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 @RestController
+@RequestMapping("/empleados")
 public class EmpleadoController {
     
     @Autowired
     private EmpleadoRepository empleadoRepository;
 
+    @GetMapping
     public List<Empleado> obtenerTodo(){
         return empleadoRepository.findAll();
     }
 
-    public Empleado guardarTodo(Empleado empleado){
+    @PostMapping
+    public Empleado guardarTodo(@RequestBody Empleado empleado){
         return empleadoRepository.save(empleado);
     }
     

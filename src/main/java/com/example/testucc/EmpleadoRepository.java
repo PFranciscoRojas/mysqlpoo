deaf5452d7d5c18c12cd181bf710d8ad26bbb086
danielartex <johndoe@example.com>
2025-03-25 19:06:20 -0500

    primer commit beach

--- src/main/java/com/example/testucc/EmpleadoRepository.java
+++ src/main/java/com/example/testucc/EmpleadoRepository.java
@@ -1,9 +1,17 @@
 package com.example.testucc;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface EmpleadoRepository extends JpaRepository<Empleado,Long> {
 
-} 
+@Repository
+public interface  EmpleadoRepository extends JpaRepository<empleado, Long>{
+
+       List<empleado> findByNombre(String nombre);
+
+       List<empleado> findBySalarioGreaterThanEqual(double salario); 
+
+       List<empleado> findByOrderBySalarioDesc();
+}

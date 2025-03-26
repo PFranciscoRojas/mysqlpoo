33ebcae686e6f7ba149fc6fc64c986abd076d27d
Jhon Moran <jhon123moran21@gmail.com>
2025-03-26 17:04:03 -0500

    jhon moran metodos

--- src/main/java/com/example/testucc/EmpleadoController.java
+++ src/main/java/com/example/testucc/EmpleadoController.java
@@ -1,21 +1,20 @@
 package com.example.testucc;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
@@ -30,53 +29,42 @@ public class EmpleadoController {
         return empleadoRepository.findAll();
     }
 
+    @GetMapping("/search")
+    public Empleado buscarPorNombre(@RequestParam String nombre) {
+        return empleadoRepository.findByNombre(nombre);
+    }
+    
+
     @PostMapping
     public Empleado guardarTodo(@RequestBody Empleado empleado){
         return empleadoRepository.save(empleado);
     }
-    
+
     @PutMapping("/{id}")
-    public Empleado actualizarEmpleado(@PathVariable String id, @RequestBody Empleado empleado) {
+    public Empleado actualizarEmpleado(@PathVariable Long id, @RequestBody Empleado empleado) {
+        empleado.setId(id);
         return empleadoRepository.save(empleado);
     }
 
-    
-    @DeleteMapping("/{id}")
-    public void eliminarEmpleado(@PathVariable String id){
-        empleadoRepository.deleteById(Long.parseLong(id));;
-    }
-    @GetMapping("/search")
-    public List<Empleado> buscarEmpleadoPorNombre(@RequestParam String nombre) {
-        return empleadoRepository.findByNombre(nombre);
-    }
-    
-    @GetMapping("/sort")
-    public List<Empleado> ordenarEmpleadosPorSalario() {
-        return empleadoRepository.findAllByOrderBySalarioDesc();
-    }
-    @PutMapping("batch/{id}")
-    public String putMethodName(@PathVariable String id, @RequestBody String entity) {
-        
-        
-    return entity;
-    }
-
     @PatchMapping("/{id}")
-    public void actualizarEmpleadoParcial(@PathVariable Long id, @RequestBody Empleado empleadoActualizado) {
-        empleadoRepository.findById(id).ifPresent(empleado -> {
-
-            if (empleadoActualizado.getNombre() != null) {
-                empleado.setNombre(empleadoActualizado.getNombre());
-            }
-            if (empleadoActualizado.getSalario() != 0.0) {
-                empleado.setSalario(empleadoActualizado.getSalario());
-            }
-
-            empleadoRepository.save(empleado);
-        });
+    public ResponseEntity<Empleado> actualizarSalario(@PathVariable Long id, @RequestBody Empleado nuevoEmpleado) {
+        Optional<Empleado> empleadoExistente = empleadoRepository.findById(id);
+        if (empleadoExistente.isPresent()) {
+            Empleado empleado = empleadoExistente.get();
+            empleado.setSalario(nuevoEmpleado.getSalario());
+            Empleado empleadoActualizado = empleadoRepository.save(empleado);
+            return ResponseEntity.ok(empleadoActualizado);
+        }
+        return null;
     }
+
     @PutMapping("/batch")
-    public List<Empleado> actualizarVarios(@RequestBody List<Empleado> empleados) {
+    public List<Empleado> actualizarEmpleados(@RequestBody List<Empleado> empleados) {
         return empleadoRepository.saveAll(empleados);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminarEmpleado(Empleado empleado){
+        empleadoRepository.delete(empleado);
     }
 }

35e4b1b433081d3ecfa904e7cf9506ad82467f9b
Cristian Ruano <ruanoperezcristian10@gmail.com>
2025-03-26 22:24:22 -0500

    Subiendo cambios

--- src/main/java/com/example/testucc/EmpleadoRepository.java
+++ src/main/java/com/example/testucc/EmpleadoRepository.java
@@ -1,17 +1,14 @@
 package com.example.testucc;
 
-
-
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface EmpleadoRepository extends JpaRepository<Empleado,Long> {
-    List<Empleado> findByNombre(String nombre);
 
-    
+@Repository
+public interface EmpleadoRepository extends JpaRepository<Empleado, Long> {
+    List<Empleado> findByNombreContaining(String nombre);
     List<Empleado> findAllByOrderBySalarioDesc();
-    
-} 
+    List<Empleado> findAllByOrderBySalarioAsc();
+}
